35d1bd054ceaad6e95d71b25ebe455fd8276da76
Daniel Monk <danielmonk@Daniels-iMac.local>
2017-09-10 13:38:25 +0100

    Gulp Install

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
 
-gulp.task('hello', function() {
-    console.log('Hello Zell');
-  });
+
+gulp.task('sass', function(){
+    return gulp.src('app/scss/**/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', ['sass']); 
+  })
+
+gulp.task('useref', function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulp.dest('dist'))
+});
